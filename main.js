59bb806a9782366f1bfaafc09fa63aca1134f857
michael.motornyi <michael.motornyi@outlook.com>
2024-03-11 01:13:44 +0200

    fix gitignore

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import './style.css'
 
 window.addEventListener("load", (event) => {
+  const imgUrl = new URL("./test.swf", import.meta.url).href;
   const ruffle = RufflePlayer.newest();
   const player = ruffle.createPlayer();
   const container = document.getElementById("container");
   container.appendChild(player);
-  player.load("./public/test.swf");
+  console.log(imgUrl);
+  player.load(imgUrl);
 });
