a8d1152aa98ff465ca60ffce39cb005f0a69e40f
michael.motornyi <michael.motornyi@outlook.com>
2024-03-11 01:03:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+window.addEventListener("load", (event) => {
+  const ruffle = RufflePlayer.newest();
+  const player = ruffle.createPlayer();
+  const container = document.getElementById("container");
+  container.appendChild(player);
+  player.load("test.swf");
+});
