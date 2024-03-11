3f667bf8bc4cdad91d0da4045cfe6415a38596e9
michael-motornyi <33175499+michael-motornyi@users.noreply.github.com>
2024-03-11 14:16:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 import './style.css'
 import binaryFileUrl from "./public/test.swf?url";
-import binaryFileStringContents from "./public/test.swf?raw";
 
 window.addEventListener("load", (event) => {
   const ruffle = RufflePlayer.newest();
   const player = ruffle.createPlayer();
-  console.log(binaryFileStringContents);
   const container = document.getElementById("container");
   container.appendChild(player);
 
   fetch(binaryFileUrl).then((data) => player.load(data))
-
 });
